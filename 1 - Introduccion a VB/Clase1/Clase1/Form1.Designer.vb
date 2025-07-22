9558a277fdc9f67bdfc378595279674d727c942a
AlejandroIngenieria <2995019300101@ingenieria.usac.edu.gt>
2025-07-22 14:18:20 -0600

    Clase 1

---
+++ 1 - Introduccion a VB/Clase1/Clase1/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtE1 = New TextBox()
+        btnE1 = New Button()
+        labelE1 = New Label()
+        labelE2 = New Label()
+        btnE2 = New Button()
+        txtE2 = New TextBox()
+        Label3 = New Label()
+        labelE3 = New Label()
+        btnE3 = New Button()
+        txtE3 = New TextBox()
+        Label4 = New Label()
+        labelE4 = New Label()
+        btnE4 = New Button()
+        txtE4 = New TextBox()
+        Label5 = New Label()
+        labelE5 = New Label()
+        btnE5 = New Button()
+        txtE5 = New TextBox()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16F)
+        Label1.Location = New Point(12, 14)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(301, 37)
+        Label1.TabIndex = 0
+        Label1.Text = "Edad minima para votar"
+        ' 
+        ' txtE1
+        ' 
+        txtE1.Location = New Point(18, 70)
+        txtE1.Name = "txtE1"
+        txtE1.Size = New Size(299, 27)
+        txtE1.TabIndex = 1
+        ' 
+        ' btnE1
+        ' 
+        btnE1.Location = New Point(337, 65)
+        btnE1.Name = "btnE1"
+        btnE1.Size = New Size(197, 36)
+        btnE1.TabIndex = 2
+        btnE1.Text = "Verificar"
+        btnE1.UseVisualStyleBackColor = True
+        ' 
+        ' labelE1
+        ' 
+        labelE1.AutoSize = True
+        labelE1.Font = New Font("Segoe UI", 16F)
+        labelE1.Location = New Point(554, 60)
+        labelE1.Name = "labelE1"
+        labelE1.Size = New Size(134, 37)
+        labelE1.TabIndex = 3
+        labelE1.Text = "Resultado"
+        ' 
+        ' labelE2
+        ' 
+        labelE2.AutoSize = True
+        labelE2.Font = New Font("Segoe UI", 16F)
+        labelE2.Location = New Point(554, 175)
+        labelE2.Name = "labelE2"
+        labelE2.Size = New Size(134, 37)
+        labelE2.TabIndex = 7
+        labelE2.Text = "Resultado"
+        ' 
+        ' btnE2
+        ' 
+        btnE2.Location = New Point(337, 180)
+        btnE2.Name = "btnE2"
+        btnE2.Size = New Size(197, 36)
+        btnE2.TabIndex = 6
+        btnE2.Text = "Verificar"
+        btnE2.UseVisualStyleBackColor = True
+        ' 
+        ' txtE2
+        ' 
+        txtE2.Location = New Point(18, 185)
+        txtE2.Name = "txtE2"
+        txtE2.Size = New Size(299, 27)
+        txtE2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 16F)
+        Label3.Location = New Point(12, 129)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(350, 37)
+        Label3.TabIndex = 4
+        Label3.Text = "Número positivo o negativo"
+        ' 
+        ' labelE3
+        ' 
+        labelE3.AutoSize = True
+        labelE3.Font = New Font("Segoe UI", 16F)
+        labelE3.Location = New Point(554, 285)
+        labelE3.Name = "labelE3"
+        labelE3.Size = New Size(134, 37)
+        labelE3.TabIndex = 11
+        labelE3.Text = "Resultado"
+        ' 
+        ' btnE3
+        ' 
+        btnE3.Location = New Point(337, 290)
+        btnE3.Name = "btnE3"
+        btnE3.Size = New Size(197, 36)
+        btnE3.TabIndex = 10
+        btnE3.Text = "Verificar"
+        btnE3.UseVisualStyleBackColor = True
+        ' 
+        ' txtE3
+        ' 
+        txtE3.Location = New Point(18, 295)
+        txtE3.Name = "txtE3"
+        txtE3.PasswordChar = "*"c
+        txtE3.Size = New Size(299, 27)
+        txtE3.TabIndex = 9
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 16F)
+        Label4.Location = New Point(12, 239)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(326, 37)
+        Label4.TabIndex = 8
+        Label4.Text = "Verificación de contraseña"
+        ' 
+        ' labelE4
+        ' 
+        labelE4.AutoSize = True
+        labelE4.Font = New Font("Segoe UI", 16F)
+        labelE4.Location = New Point(560, 395)
+        labelE4.Name = "labelE4"
+        labelE4.Size = New Size(134, 37)
+        labelE4.TabIndex = 15
+        labelE4.Text = "Resultado"
+        ' 
+        ' btnE4
+        ' 
+        btnE4.Location = New Point(343, 400)
+        btnE4.Name = "btnE4"
+        btnE4.Size = New Size(197, 36)
+        btnE4.TabIndex = 14
+        btnE4.Text = "Verificar"
+        btnE4.UseVisualStyleBackColor = True
+        ' 
+        ' txtE4
+        ' 
+        txtE4.Location = New Point(24, 405)
+        txtE4.Name = "txtE4"
+        txtE4.Size = New Size(299, 27)
+        txtE4.TabIndex = 13
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 16F)
+        Label5.Location = New Point(18, 349)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(335, 37)
+        Label5.TabIndex = 12
+        Label5.Text = "Verificación de número par"
+        ' 
+        ' labelE5
+        ' 
+        labelE5.AutoSize = True
+        labelE5.Font = New Font("Segoe UI", 16F)
+        labelE5.Location = New Point(560, 519)
+        labelE5.Name = "labelE5"
+        labelE5.Size = New Size(134, 37)
+        labelE5.TabIndex = 19
+        labelE5.Text = "Resultado"
+        ' 
+        ' btnE5
+        ' 
+        btnE5.Location = New Point(343, 524)
+        btnE5.Name = "btnE5"
+        btnE5.Size = New Size(197, 36)
+        btnE5.TabIndex = 18
+        btnE5.Text = "Verificar"
+        btnE5.UseVisualStyleBackColor = True
+        ' 
+        ' txtE5
+        ' 
+        txtE5.Location = New Point(24, 529)
+        txtE5.Name = "txtE5"
+        txtE5.Size = New Size(299, 27)
+        txtE5.TabIndex = 17
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 16F)
+        Label6.Location = New Point(18, 473)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(153, 37)
+        Label6.TabIndex = 16
+        Label6.Text = "Calificacion"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1037, 718)
+        Controls.Add(labelE5)
+        Controls.Add(btnE5)
+        Controls.Add(txtE5)
+        Controls.Add(Label6)
+        Controls.Add(labelE4)
+        Controls.Add(btnE4)
+        Controls.Add(txtE4)
+        Controls.Add(Label5)
+        Controls.Add(labelE3)
+        Controls.Add(btnE3)
+        Controls.Add(txtE3)
+        Controls.Add(Label4)
+        Controls.Add(labelE2)
+        Controls.Add(btnE2)
+        Controls.Add(txtE2)
+        Controls.Add(Label3)
+        Controls.Add(labelE1)
+        Controls.Add(btnE1)
+        Controls.Add(txtE1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtE1 As TextBox
+    Friend WithEvents btnE1 As Button
+    Friend WithEvents labelE1 As Label
+    Friend WithEvents labelE2 As Label
+    Friend WithEvents btnE2 As Button
+    Friend WithEvents txtE2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents labelE3 As Label
+    Friend WithEvents btnE3 As Button
+    Friend WithEvents txtE3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents labelE4 As Label
+    Friend WithEvents btnE4 As Button
+    Friend WithEvents txtE4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents labelE5 As Label
+    Friend WithEvents btnE5 As Button
+    Friend WithEvents txtE5 As TextBox
+    Friend WithEvents Label6 As Label
+
+End Class
---
+++ 1 - Introduccion a VB/Clase1/Clase1/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub btnE1_Click(sender As Object, e As EventArgs) Handles btnE1.Click
+        'Es un comentario de una sola línea
+        'Declaración de variables
+        Dim edad As Integer
+        'Declaración de variables de tipo String
+        edad = txtE1.Text
+        'Uso de condicionales
+        If edad >= 18 Then
+            labelE1.Text = "Eres mayor de edad"
+        Else
+            labelE1.Text = "Eres menor de edad"
+        End If
+    End Sub
+
+    Private Sub btnE2_Click(sender As Object, e As EventArgs) Handles btnE2.Click
+        'Declaración de variables
+        Dim numero As Integer
+        'Almacenar el contenido del TextBox en la variable
+        numero = txtE2.Text
+        'Condicion para saber si es posivito o negativo
+        If numero >= 0 Then
+            labelE2.Text = "El numero es positivo"
+        Else
+            labelE2.Text = "El numero es negativo"
+        End If
+    End Sub
+
+    Private Sub btnE3_Click(sender As Object, e As EventArgs) Handles btnE3.Click
+        'Declarar una variable para guardar la contraseña
+        Dim password As String
+
+        'Almacenar el contenido del TextBox en la variable
+        password = txtE3.Text
+        'Condición para verificar la contraseña
+        'Sintaxis de If
+        'If condición Then <- obligatorio
+        'Codigo que quieres que se ejecute si la condición es verdadera <- obligatorio
+        'Else <- opcional
+        'Codigo que quieres que se ejecute si la condición es falsa <- opcional
+        'End If <- obligatorio
+        If password = "1234" Then
+            labelE3.Text = "Acceso condedido"
+        Else
+            labelE3.Text = "Acceso no concedido"
+        End If
+    End Sub
+
+    Private Sub btnE4_Click(sender As Object, e As EventArgs) Handles btnE4.Click
+        Dim numero As Double
+        numero = txtE4.Text
+        'Mod lo que nos devuelve es el residuo de la división
+        If numero Mod 2 = 0 Then
+            labelE4.Text = "Es par"
+            MsgBox(numero Mod 2)
+        Else
+            labelE4.Text = "Es impar"
+            MsgBox(numero Mod 2)
+        End If
+    End Sub
+
+    Private Sub btnE5_Click(sender As Object, e As EventArgs) Handles btnE5.Click
+        Dim calificacion As Integer
+        calificacion = txtE5.Text
+        If calificacion >= 61 And calificacion < 100 Then
+            MsgBox("Aprobado")
+        ElseIf calificacion > 100 Then
+            MsgBox("Calificación no válida")
+        Else
+            MsgBox("Reprobado")
+        End If
+    End Sub
+End Class
